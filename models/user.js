fefa9ff0de3e4f8d22aeb8aa559d7a5fcd748612
shame-wizards-apprentice <rachelnelsonschille@gmail.com>
2021-01-30 09:10:18 -0800

    removed name column from users table, replaced with username

--- models/user.js
+++ models/user.js
@@ -7,6 +7,11 @@ module.exports = (sequelize, DataTypes) => {
             type: DataTypes.STRING,
             allowNull: false
         },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
         email: {
             type: DataTypes.STRING,
             allowNull: false,
--- server.js
+++ server.js
@@ -23,7 +23,7 @@ var routes = require("./controllers/usersController.js");
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
