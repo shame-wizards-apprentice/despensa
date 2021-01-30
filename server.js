cd39a78e01ed036272defa3827def3bfd8a8839a
shame-wizards-apprentice <rachelnelsonschille@gmail.com>
2021-01-30 09:55:03 -0800

    commented out all routes except signup for testing purposes

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-var express = require("express");
+const express = require("express");
 
-var PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
-var app = express();
+const app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Set Handlebars.
-var exphbs = require("express-handlebars");
+const exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
@@ -23,13 +23,11 @@ const db = require("./models");
 // Import routes and give the server access to them.
 const userRoutes = require("./controllers/usersController.js");
 
-
 app.use(userRoutes);
 
 // Start our server so that it can begin listening to client requests.
 db.sequelize.sync({ force: false }).then(function () {
   app.listen(PORT, function () {
-    // Log (server-side) when our server has started
     console.log("Server listening on: http://localhost:" + PORT);
   });
 });
