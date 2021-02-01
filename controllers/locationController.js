ce0b8714d59da00396771d79cea5c83f1ce84389
Douglas Johnson <trunks22a@gmail.com>
2021-01-31 22:46:53 -0800

    stash

--- controllers/locationController.js
+++ controllers/locationController.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-const express = require("express");
-const router = express.Router();
-
-const food = require("../models/food.js");
-
-// // Import the model (location.js) to use its database functions.
-const { Location } = require("../models");
-
-// // Create all our routes and set up logic within those routes where required.
-router.get("/", (req, res) => {
-    res.render("index", {});
-=======
 // const express = require("express");
 // const food = require("../models/food.js");
 
@@ -22,32 +9,9 @@ router.get("/", (req, res) => {
 // // Create all our routes and set up logic within those routes where required.
 // router.get("/", function (req, res) {
 //     res.redirect("/locations/:user_id")
->>>>>>> dev
 
 // });
 
-<<<<<<< HEAD
-// Display all locations owned by user
-router.get("/locations/:user_id", (req, res) => {
-    if (!req.sessions.user) {
-        res.status(401).send("Nice try, idiot")
-    }
-    else {
-        Location.findAll({
-            where: {
-                user_id: req.sessions.user_id
-            }
-        }).then(data => {
-            console.log(data);
-            const dbLocationJson = data.map(location => location.toJSON())
-            var hbsObject = { location: dbLocationJson };
-            return res.render("index", hbsObject);
-        })
-
-    }
-
-});
-=======
 // router.get("/locations/:user_id", (req, res) => {
 //     Location.findAll({
 //         where: {
@@ -60,86 +24,9 @@ router.get("/locations/:user_id", (req, res) => {
 //         return res.render("index", hbsObject);
 //     })
 // });
->>>>>>> dev
 
 // Create a new location if user is logged in
 router.post("/locations/create", (req, res) => {
-<<<<<<< HEAD
-    if (!req.session.user) {
-        res.status(401).send("Nice try, idiot")
-    }
-    else {
-        Location.create({
-            name: req.body.name,
-            type: req.body.type,
-            user_id: req.session.user.id
-        }).then(data => {
-            console.log(data);
-            res.send(data);
-            res.redirect("/");
-        }).catch(err => {
-            if (err) console.log(err.message)
-            res.status(500).send("Internal server error")
-        });
-
-    }
-
-});
-
-// Update user's locations if logged in
-router.put("/locations/update/:id", (req, res) => {
-    if (!req.sessions.user) {
-        res.status(401).send("Nice try, idiot")
-    }
-    else {
-        Location.update(
-            {
-                name: req.body.name,
-                type: req.body.type,
-                user_id: req.session.user.id
-            },
-            {
-                where: {
-                    id: req.body.id
-                }
-            }
-        ).then(data => {
-            res.send(data);
-            res.redirect("/");
-        }).catch(err => {
-            if (err) console.log(err.message)
-            res.status(500).send("Internal server error")
-        });
-
-    }
-
-});
-
-// Delete a location, if user is logged in
-router.delete("locations/delete/:id", (req, res) => {
-    if (!req.sessions.user) {
-        res.status(401).send("Nice try, idiot")
-    }
-    else {
-        Location.destroy({
-            where: {
-                user_id: req.sessions.user.id,
-                id: req.params.id
-            }
-        }).then(data => {
-            res.send(data);
-            res.redirect("/");
-        }).catch(err => {
-            if (err) console.log(err.message)
-            res.status(500).send("Internal server error")
-        });
-
-    }
-
-});
-// // Export routes for server.js to use.
-module.exports = router;
-=======
     if(!req.session.user){
         res.status(401).send("Please login first.")
     }
@@ -187,4 +74,3 @@ module.exports = router;
 // });
 // // Export routes for server.js to use.
 // module.exports = router;
->>>>>>> dev
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -3,20 +3,6 @@ const express = require("express");
 const bcrypt = require("bcrypt");
 const db = require("../models");
 
-<<<<<<< HEAD
-// Express router methods 
-const router = express.Router();
-
-// Routes
-// Homepage route
-router.get('/', (req, res) => {
-    res.render("index", {});
-});
-
-// Signup route
-router.post("/signup", (req, res) => {
-    db.User.create({
-=======
 const router = express.Router();
 
 // Import the model (User.js) to use its database functions.
@@ -32,69 +18,10 @@ router.get('/', function(req, res) {
 // Create Route
 router.post("/signup", function (req, res) {
     User.create({
->>>>>>> dev
         username: req.body.username,
         email: req.body.email,
         password: req.body.password,
         theme_id: req.body.theme_id
-<<<<<<< HEAD
-    }),
-    db.Location.create(
-        {
-        name: "Shopping list",
-        type: list,
-        },
-        {
-            name: "Pantry",
-            type: pantry
-        },
-    ),
-    db.Container.create(
-        {
-            type: shelf,
-        },
-        {
-          type: drawer,  
-        }
-    )
-    .then((data) => {
-    }).then(data => {
-        res.json(data)
-    }).catch(err => {
-        res.status(500).send(err.message);
-    });
-});
-
-// Login route
-router.post("/login", (req, res) => {
-    db.User.findOne({
-        where: {
-            username: req.body.username
-        }
-    }).then(data => {
-        if (!data) {
-            res.status(404).send("user does not exist...on this app.")
-        } else {
-            if (bcrypt.compareSync(req.body.passowrd, data.password)) {
-                req.session.user = {
-                    id: data.id,
-                    username: data.username
-                }
-                res.json(data);
-            } else {
-                res.status(401).send("Ah ah ah! You didn't say the magic word...")
-            }
-        }
-    }).catch(err => {
-        if (err) console.log(err.message)
-        res.status(500).send("Internal server error")
-    });
-
-});
-
-// Sessions route
-router.get("/readsessions", (req, res) => {
-=======
     }).then((data) => {
         res.json(data)
     }).catch(err => {
@@ -128,20 +55,13 @@ router.post("/login", function (req, res) {
 
 // Sessions Route
 router.get("/readsessions",(req,res)=>{
->>>>>>> dev
     res.json(req.session)
 })
 
 // Test Route
-<<<<<<< HEAD
-router.get("/lardersquadVIP", (req, res) => {
-    if (req.session.user) {
-        res.send(`    You are part of an exceptional group of people, ${req.session.user.username}.`)
-=======
 router.get("/lardersquadVIP", (req,res)=> {
     if(req.session.user) {
         res.send(`You are part of an exceptional group of people, ${req.session.user.username}.`)
->>>>>>> dev
     } else {
         res.status(401).send("login required.")
     }
