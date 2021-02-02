48fbca315a3047428f60bba09c60e18777a170c9
Douglas Johnson <trunks22a@gmail.com>
2021-02-02 10:15:02 -0800

    401+500

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -9,32 +9,32 @@ const router = express.Router();
 const { User } = require('../models');
 
 router.get("/", (req, res) => {
-	res.render("index", {username: "Angel", email: "skelliebunnie@gmail.com", theme: "metro"});
+  res.render("add-food", { username: "Angel", email: "skelliebunnie@gmail.com", theme: "metro" });
 });
 
 // Create Route
-router.post("/api/signup", function(req, res) {
+router.post("/api/signup", function (req, res) {
   User.create({
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password,
-      theme_id: req.body.theme_id
-    })
-  // ,
-  //   db.Location.create({
-  //     name: "Shopping list",
-  //     type: "list",
-  //   }, {
-  //     name: "Pantry",
-  //     type: "pantry"
-  //   },
-  //   ),
-  //   db.Container.create({
-  //     type: "shelf",
-  //   }, {
-  //     type: "drawer",
-  //   })
-    .then((data) => {}).then(data => {
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+    theme_id: req.body.theme_id
+  })
+    // ,
+    //   db.Location.create({
+    //     name: "Shopping list",
+    //     type: "list",
+    //   }, {
+    //     name: "Pantry",
+    //     type: "pantry"
+    //   },
+    //   ),
+    //   db.Container.create({
+    //     type: "shelf",
+    //   }, {
+    //     type: "drawer",
+    //   })
+    .then((data) => { }).then(data => {
       res.json(data)
     }).catch(err => {
       res.status(500).send(err.message);
@@ -57,7 +57,7 @@ router.post("/api/login", (req, res) => {
           username: data.username
         }
         // res.json(data);
-        res.render("profile", {user: req.session.user});
+        res.render("profile", { user: req.session.user });
       } else {
         res.status(401).send("Ah ah ah! You didn't say the magic word...")
       }
@@ -97,7 +97,7 @@ router.get("/lardersquadVIP", (req, res) => {
 
 // Update Route
 router.put("/api/users/update/:id", (req, res) => {
-	console.log(req.body);
+  console.log(req.body);
   let userObj = {};
   if (req.body.username !== "" && req.body.username !== null) {
     userObj.username = req.body.username;
@@ -113,10 +113,10 @@ router.put("/api/users/update/:id", (req, res) => {
   }
   User.update(
     userObj, {
-      where: {
-        id: req.params.id
-      }
-    }).then((data) => {
+    where: {
+      id: req.params.id
+    }
+  }).then((data) => {
     console.log(data)
     res.send("User info updated.");
   }).catch(err => {
